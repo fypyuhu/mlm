5de595603beb91f9b622b646e826763b6c4f11f1
muhammad usama riaz <muhammadusamariaz@gmail.com>
2016-03-21 00:43:11 +0500

    image added

--- app/views/template/mlm/layout1.blade.php
+++ app/views/template/mlm/layout1.blade.php
@@ -270,26 +270,26 @@ IN vary simple words just by shopping you can earn upto 11000usd.
 <div class="box">
 <div class="center">
 <h2>Meet the Team</h2>
-<p class="lead">Pellentesque habitant morbi tristique senectus et netus et<br>malesuada fames ac turpis egestas.</p>
+<p class="lead">Our Great Team Members.We Hired only Professionals.</p>
 </div>
 <div class="gap"></div>
 <div id="team-scroller" class="carousel scale">
 <div class="carousel-inner">
 <div class="item active">
 <div class="row">
-<div class="col-sm-4">
+<div class="col-sm-3">
 <div class="member">
-<p><img class="img-responsive img-thumbnail img-circle" src="images/team1.jpg" alt=""></p>
-<h3>Agnes Smith<small class="designation">CEO &amp; Founder</small></h3>
+<p><img class="img-responsive img-thumbnail img-circle" src="/images/members/fiaz.jpg" alt=""></p>
+<h3>Faiz Hussain<small class="designation">CEO &amp; Founder</small></h3>
 </div>
 </div>
-<div class="col-sm-4">
+<div class="col-sm-3">
 <div class="member">
-<p><img class="img-responsive img-thumbnail img-circle" src="images/team2.jpg" alt=""></p>
-<h3>Donald Ford<small class="designation">Senior Vice President</small></h3>
+<p><img class="img-responsive img-thumbnail img-circle" src="/images/members/fiaz.jpg" alt=""></p>
+<h3>Faiz Hussain<small class="designation">CEO &amp; Founder</small></h3>
 </div>
 </div>
-<div class="col-sm-4">
+<div class="col-sm-3">
 <div class="member">
 <p><img class="img-responsive img-thumbnail img-circle" src="images/team3.jpg" alt=""></p>
 <h3>Karen Richardson<small class="designation">Assitant Vice President</small></h3>
@@ -299,19 +299,19 @@ IN vary simple words just by shopping you can earn upto 11000usd.
 </div>
 <div class="item">
 <div class="row">
-<div class="col-sm-4">
+<div class="col-sm-3">
 <div class="member">
 <p><img class="img-responsive img-thumbnail img-circle" src="images/team3.jpg" alt=""></p>
 <h3>David Robbins<small class="designation">Co-Founder</small></h3>
 </div>
 </div>
-<div class="col-sm-4">
+<div class="col-sm-3">
 <div class="member">
 <p><img class="img-responsive img-thumbnail img-circle" src="images/team1.jpg" alt=""></p>
 <h3>Philip Mejia<small class="designation">Marketing Manager</small></h3>
 </div>
 </div>
-<div class="col-sm-4">
+<div class="col-sm-3">
 <div class="member">
 <p><img class="img-responsive img-thumbnail img-circle" src="images/team2.jpg" alt=""></p>
 <h3>Charles Erickson<small class="designation">Support Manager</small></h3>
@@ -322,7 +322,7 @@ IN vary simple words just by shopping you can earn upto 11000usd.
 </div>
 <a class="left-arrow" href="#team-scroller" data-slide="prev">
 <i class="icon-angle-left icon-4x"></i>
-</a>
+</a>    
 <a class="right-arrow" href="#team-scroller" data-slide="next">
 <i class="icon-angle-right icon-4x"></i>
 </a>
